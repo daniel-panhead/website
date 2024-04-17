25aa8741ac60f05798f278ed0dd1b9cb8f38c4d2
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-04-16 17:07:22 -0700

    Add more projects and don't ssr project cards due to dependence on client breakpoints

--- src/pages/_projects.ts
+++ src/pages/_projects.ts
@@ -20,5 +20,27 @@ export const projects = [
     prototype generator app, which can help startup founders quickly
     make mockups for their ideas.
     `
+  },
+  {
+    title: "UBC BIOMOD 2023",
+    tagline: "Website for UBC BIOMOD's 2023 project submission",
+    link: "#",
+    description: `
+    ProFound was a project we made for UBC BizTech's 2024 ProduHacks. In
+    under 24 hours, I designed the frontend for an AI-powered low-code
+    prototype generator app, which can help startup founders quickly
+    make mockups for their ideas.
+    `
+  },
+  {
+    title: "Dr. Sprouts",
+    tagline: "The AI doctor for your plants!",
+    link: "#",
+    description: `
+    ProFound was a project we made for UBC BizTech's 2024 ProduHacks. In
+    under 24 hours, I designed the frontend for an AI-powered low-code
+    prototype generator app, which can help startup founders quickly
+    make mockups for their ideas.
+    `
   }
 ];
