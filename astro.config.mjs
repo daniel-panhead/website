7b191bdfed09f5143eba8e35ae1cf4846ee01e51
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-04-19 18:55:04 -0700

    Add partytown for gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danielpanhead.com',
-  integrations: [vue()]
+  integrations: [
+    vue(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ]
 });
